d0998d9cd46a97189da6669dc497eed773eaeb28
alejandrogobu <alejandro.gonzalez.bu@gmail.com>
2025-02-12 18:14:15 +0100

    test deploy dbt pro

--- dagster_university/project.py
+++ dagster_university/project.py
@@ -3,8 +3,7 @@ from pathlib import Path
 from dagster_dbt import DbtProject
 
 dbt_project = DbtProject(
-   project_dir=Path(__file__).joinpath("..", "..", "analytics").resolve(),
-   target=os.getenv("DBT_TARGET")
+   project_dir=Path(__file__).joinpath("..", "..", "analytics").resolve()
 )
 
 dbt_project.prepare_if_dev()
